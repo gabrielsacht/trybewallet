ef633d53c1f6ec9ce70911a16e9a5d3c515480f0
gabrielsacht <gabrielsacht@users.noreply.github.com>
2022-08-03 02:34:54 +0000

    deploy: 0528e7a1b314514179cb8956cfd69a0484dfc3e9

---
+++ precache-manifest.b964f4fd068d1b8256ed54f368aa1374.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d3846ff4d089c22728ec9125b92fdc",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "f9378a226d4e0132732e",
+    "url": "/trybewallet/static/css/main.1854a9bd.chunk.css"
+  },
+  {
+    "revision": "6c04d8eb595960cc1393",
+    "url": "/trybewallet/static/js/2.ce1bd4b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.ce1bd4b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9378a226d4e0132732e",
+    "url": "/trybewallet/static/js/main.605f8b9d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/trybewallet/static/js/main.605f8b9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  }
+]);
